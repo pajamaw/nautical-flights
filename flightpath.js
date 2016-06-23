520572793ead337bca3ecaa146a8ae2d6c777c95
pajamaw <pjpeterww@gmail.com>
2016-06-23 13:00:11 -0400

    finished

--- flightpath.js
+++ flightpath.js
@@ -1,7 +1,8 @@
-var map;
-function initMap() {
+var map; //setting map global
+var marker;
+function initMap() { //creating a new map to be used for the app
 map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 37.0902, lng: -95.7129},
+    center: {lat: 37.0902, lng: -95.7129}, //usa
     zoom: 4
   });
 };
@@ -18,67 +19,92 @@ document.addEventListener("DOMContentLoaded", function(event){
     flightPoints.pop();
     distancePoints.pop();
   };
-  function removeAll(){
+  function removeAll(){ //used to reset map after button click or additional point requested
     flightPoints = [];
     distancePoints = [];
     initMap();
-    document.getElementsByClassName('autocomplete1').value="";
-    document.getElementById('autocomplete2').value="";
+    clearInputs();
     document.getElementById('distance').textContent="0"
     distance = 0;
     flightPath = 0;
-    document.getElementById('autocomplete2').value="";
-
   };
-  function checkFlightPath(fPoints, dPoints, m){
+  function clearInputs(){
+    document.getElementById('autocomplete1').value="";
+    document.getElementById('autocomplete2').value="";
+  };
+  function checkFlightPath(fPoints, dPoints, m){//to ensure that polylines only are created when there are single points
     if(fPoints.length > 1){
       createFlightPath(fPoints, dPoints, m);
     };
   }
-  function createFlightPath(fPoints, dPoints, m){
+  function panToMarker(fPoints, m){//prevents markers from being panned to when there are already 2 markers on the page
+    if (fPoints.length < 3){
+      map.panTo(m.coor);
+    };
+  }
+  function createFlightPath(fPoints, dPoints, m){//creates geodesic polyline
     if(fPoints.length == 2){
       var distance = google.maps.geometry.spherical.computeDistanceBetween(dPoints[0], dPoints[1]);
       document.getElementById("distance").textContent=convertToSeaMiles(distance);
       var flightPath = new google.maps.Polyline({
         map: map,
-        path: fPoints,//flightPlanCoordinates,
+        path: fPoints,
         geodesic: true,
         strokeColor: '#FF0000',
         strokeOpacity: 1.0,
         strokeWeight: 2
       });
-      //console.log(convertToSeaMiles(distance)+ " in nautical miles")
-      //debugger;
-    }else{ //fPoints.length > 2)
+    }else{
         var reset = confirm("Would you like to reset the form and check a new distance?");
-        if(reset != true){
-          m.getMap(null);
+        if(reset != true){//if there is a 3rd marker it asks you to reset the page to create a new
+                          //a new path or it will negate latest marker
+          m.setMap(null);
+        }else{
+          removeAll();//creates new map to begin new path
+
         }
-        removeAll();
     }
   };
+  function setMarker(selectedClick, dPoints, fPoints){
+    marker = new google.maps.Marker({//sets marker using new cooridnates
+      map: map,
+      animation: google.maps.Animation.DROP,
+      position: selectedClick.item.LatLng,
+      coor: {
+        lat: selectedClick.item.LatLng.lat,
+        lng: selectedClick.item.LatLng.lng
+      },
+      label: selectedClick.item.label
+    });
+    distancePoints.push(marker.position);
+    flightPoints.push(marker.coor);
+    panToMarker(fPoints, marker);
+    checkFlightPath(fPoints, dPoints, marker);
+  };
 
-  $('.autocomplete').autocomplete({
-      source: function( request, response ) {
-          $.ajax({
+  $('.autocomplete').autocomplete({//autocomplete ajax function
+      source: function(request, response) {
+          $.ajax({//ajax request using airport codes api to grab all the airport codes for the world
               url: "//www.air-port-codes.com/search/",
               jsonp: "callback",
               dataType: "jsonp",
               data: {
                   term: request.term, // input field value
-                  limit: 100, // default is 30
-                  size: 0, // default is 0
-                  key: "b34e0b6ea2", // dont forget to add your API Key from your air-port-codes account
+                  limit: 100,
+                  size: 0,
+                  key: "b34e0b6ea2",
                   secret: "ce36bb996d28c2c"
               },
-              success: function( data ) {
+              success: function(data) {
                 //debugger;
                   if (data.status) { // success
-                      response( data.airports.filter(function (airport) {
+                      response(data.airports.filter(function (airport) {
+                        //filters through airports to only use US airport locations that
+                        //have valid lat and lng
                         return airport.country.iso === "US" && airport.latitude !== null;
                       }).map(function(ap) {
                           return {
-                              label: ap.name +' (' + ap.iata + ')',
+                              label: ap.name, //autcompleted name
                               state: ap.state.abbr,
                               LatLng: {
                                 lat: parseFloat(ap.latitude),
@@ -93,30 +119,8 @@ document.addEventListener("DOMContentLoaded", function(event){
           });
       },
       select: function(event, selected) {
-          console.log(selected);
-          console.log(selected.item);
-          console.log(selected.item.location);
-          var marker = new google.maps.Marker({
-            map: map,
-            animation: google.maps.Animation.DROP,
-            position: selected.item.LatLng,
-            coor: {
-              lat: selected.item.LatLng.lat,
-              lng: selected.item.LatLng.lng
-            },
-            label: selected.item.label,
-            title: "wooot"//ui.item.label
-          });
-
-          distancePoints.push(marker.position);
-          flightPoints.push(marker.coor);
-
-          console.log(flightPoints.length);
-          checkFlightPath(flightPoints);
-
+        setMarker(selected, distancePoints, flightPoints);
       }
-
   });
 
 });
-//document.write('\//<SCR'+'IPT src=https://maps.googleapis.com/maps/api/js?key=AIzaSyA_5h7UwDWuzihCPUwd8FA_NKgEJM30URk&callback=initMap><\/SCR'+'IPT>');
